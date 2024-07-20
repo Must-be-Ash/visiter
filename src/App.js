e0b5c7ac93e77e02e5f111992c13dc99521b5a6b
Ash <105402607+Must-be-Ash@users.noreply.github.com>
2024-07-19 18:19:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Here&rsquo;s how many people have sat there before you!</h1>
+        <h1>Here&rsquo;s how many people </h1>
+        <h1>have sat here before you!</h1>
         <div className="counter">{count}</div>
         <p>that&#39;s a lot of asses</p>
     
